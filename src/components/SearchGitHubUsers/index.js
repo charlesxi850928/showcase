1248903624efd0df272d1f18c936fced38e2027b
Charles Xi <charlesx@yeah.net>
2021-10-23 22:07:24 +0800

    CXI10231107 Initial

---
+++ src/components/SearchGitHubUsers/index.js
@@ -0,0 +1,7 @@
+import { Grid } from "@mui/material"
+
+const SearchGitHubUsers = () => {
+    return <Grid></Grid>
+}
+
+export default SearchGitHubUsers
---
+++ src/components/ShowCase.js
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const ShowCase = () =>{
+    return (
+        <Grid>
+            <Link className='App-link'>To Do List</Link>
+        </Grid>
+    )
+}
+
+export default ShowCase
