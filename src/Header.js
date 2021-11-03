f07d3468dab28fe518354f9655c68363de88679b
cxi <Charles.Xi@aspiraconnect.com>
2021-11-03 13:52:05 +0800

    CXI202111031351 Home UI Change

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import {useHistory} from 'react-router-dom'
 import {Avatar, Grid, Paper, Typography} from '@mui/material'
 import logo from './logo.svg'
 
-const HomeLink = () => {
+const Header = () => {
   const history = useHistory()
   const back = () => {
     history.goBack()
@@ -14,46 +14,43 @@ const HomeLink = () => {
     history.goForward()
   }
   return (
-    <Grid display='flex' alignItems='center' role='region' aria-label='header info'>
-      <Typography sx={visuallyHidden} id='sr-only-page-title' variant='h1' component='h1' aria-live='polite'>
-        Show Case
-      </Typography>
-      <a className='App-link' href='/'>
-        <img src={logo} style={{width: 100}} alt='logo' />
-      </a>
-      <Avatar
-        onClick={back}
-        alt='Back to previous page'
-        sx={{
-          bgcolor: deepPurple[600],
-          width: 52,
-          height: 52,
-          cursor: 'pointer',
-          marginRight: 2.5
-        }}
-      >
-        BK
-      </Avatar>
-      <Avatar
-        onClick={forward}
-        sx={{
-          bgcolor: blue[900],
-          width: 52,
-          height: 52,
-          cursor: 'pointer'
-        }}
-      >
-        PV
-      </Avatar>
-    </Grid>
+    <Paper>
+      <Grid display='flex' alignItems='center' role='region' aria-label='header info'>
+        <Typography sx={visuallyHidden} id='sr-only-page-title' variant='h1' component='h1' aria-live='polite'>
+          Show Case
+        </Typography>
+        <a className='App-link' href='/'>
+          <img src={logo} style={{width: 100}} alt='logo' />
+        </a>
+        <Avatar
+          onClick={back}
+          alt='Back to previous page'
+          sx={{
+            bgcolor: deepPurple[600],
+            width: 52,
+            height: 52,
+            cursor: 'pointer',
+            marginRight: 2.5
+          }}
+        >
+          BK
+        </Avatar>
+        <Avatar
+          onClick={forward}
+          sx={{
+            bgcolor: blue[900],
+            width: 52,
+            height: 52,
+            cursor: 'pointer'
+          }}
+        >
+          PV
+        </Avatar>
+      </Grid>
+    </Paper>
   )
 }
-HomeLink.propTypes = {}
-const Header = () => (
-  <Paper>
-    <HomeLink />
-  </Paper>
-)
+
 Header.propTypes = {}
 
 export default Header
