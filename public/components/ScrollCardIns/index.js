c2eb5ca6098b0fec314018fe25b04379353462cd
Charles Xi <charlesx@yeah.net>
2021-11-07 20:50:27 +0800

    CXI11072050 Images Change

--- public/components/ScrollCardIns/index.js
+++ public/components/ScrollCardIns/index.js
@@ -5,103 +5,103 @@ const ScrollCardIns = () => {
   const cardInfoList = [
     {
       id: '1',
-      imgUrl: 'assets/images/scroll-card/1.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_1.jpg',
       title: 'Composite image of southern Africa',
       description: `Composite image of southern Africa and the surrounding oceans 
           captured by six orbits of the NASA/NOAA Suomi National Polar-orbiting Partnership spacecraft`
     },
     {
       id: '2',
-      imgUrl: 'assets/images/scroll-card/2.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_2.jpg',
       title: 'Hubble Space Telescope',
       description: `Hubble Space Telescope, the Spitzer Space Telescope, and the Chandra X-ray 
           Observatory produced a matched trio of images of the central region of our Milky Way galaxy`
     },
     {
       id: '3',
-      imgUrl: 'assets/images/scroll-card/3.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_3.jpg',
       title: 'Hubble Space Telescope',
       description: 'Hubble space telescope captures vivid auroras in Jupiter’s atmosphere'
     },
     {
       id: '4',
-      imgUrl: 'assets/images/scroll-card/4.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_4.jpg',
       title: 'Mast Camera (Mastcam)',
       description: `This view from the Mast Camera (Mastcam) in NASA’s Curiosity Mars rover shows an 
           outcrop with finely layered rocks within the ‘Murray Buttes’ region on lower Mount Sharp`
     },
     {
       id: '5',
-      imgUrl: 'assets/images/scroll-card/5.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_5.jpg',
       title: 'Mystic Mountain',
       description: 'Hubble space telescope captures Mystic Mountain in the Carina Nebula'
     },
     {
       id: '6',
-      imgUrl: 'assets/images/scroll-card/6.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_6.jpg',
       title: 'Jupiter A',
       description: `This view of Jupiter was taken by Voyager 1. This image was taken through color filters 
           and recombined to produce the color image`
     },
     {
       id: '7',
-      imgUrl: 'assets/images/scroll-card/7.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_7.jpg',
       title: 'Jupiter B',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '8',
-      imgUrl: 'assets/images/scroll-card/8.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_8.jpg',
       title: 'Jupiter C',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '9',
-      imgUrl: 'assets/images/scroll-card/9.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_9.jpg',
       title: 'Jupiter D',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '10',
-      imgUrl: 'assets/images/scroll-card/10.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_10.jpg',
       title: 'Jupiter E',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '11',
-      imgUrl: 'assets/images/scroll-card/11.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_11.jpg',
       title: 'Jupiter F',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '12',
-      imgUrl: 'assets/images/scroll-card/12.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_12.jpg',
       title: 'Jupiter G',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '13',
-      imgUrl: 'assets/images/scroll-card/13.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_13.jpg',
       title: 'Jupiter H',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '14',
-      imgUrl: 'assets/images/scroll-card/14.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_14.jpg',
       title: 'Jupiter I',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: '15',
-      imgUrl: 'assets/images/scroll-card/15.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_15.jpg',
       title: 'Jupiter J',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
--- src/components/TabScrollCardIns/index.js
+++ src/components/TabScrollCardIns/index.js
@@ -6,87 +6,87 @@ const ScrollCardIns = () => {
   const cardInfoList = [
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/1.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_1.jpg',
       title: 'Composite image of southern Africa'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/2.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_2.jpg',
       title: 'Hubble Space Telescope'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/3.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_3.jpg',
       title: 'Hubble Space Telescope'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/4.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_4.jpg',
       title: 'Mast Camera (Mastcam)',
       description: `This view from the Mast Camera (Mastcam) in NASA’s Curiosity Mars rover shows an 
           outcrop with finely layered rocks within the ‘Murray Buttes’ region on lower Mount Sharp`
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/5.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_5.jpg',
       title: 'Mystic Mountain',
       description: 'Hubble space telescope captures Mystic Mountain in the Carina Nebula'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/6.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_6.jpg',
       title: 'Jupiter A',
       description: `This view of Jupiter was taken by Voyager 1. This image was taken through color filters 
           and recombined to produce the color image`
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/7.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_7.jpg',
       title: 'Jupiter B'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/8.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_8.jpg',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/9.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_9.jpg',
       title: 'Jupiter D',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/10.jpg'
+      imgUrl: 'assets/images/scroll-card/300x200_10.jpg'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/11.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_11.jpg',
       title: 'Jupiter F'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/12.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_12.jpg',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/13.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_13.jpg',
       title: 'Jupiter H'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/14.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_14.jpg',
       title: 'Jupiter I',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
     },
     {
       id: nanoid(),
-      imgUrl: 'assets/images/scroll-card/15.jpg',
+      imgUrl: 'assets/images/scroll-card/300x200_15.jpg',
       title: 'Jupiter J',
       description:
         'This view of Jupiter was taken by Voyager 1. This image was taken through color filters and recombined to produce the color image'
